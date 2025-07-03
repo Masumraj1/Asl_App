5c407c67445c6e9013130faa947ca76933a71e3e
masum <promotionbd321@gmail.com>
2025-07-03 19:58:24 +0600

    home screen complete

--- lib/app/services/api_client.dart
+++ lib/app/services/api_client.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/request/request.dart';
 import 'package:http/http.dart' as http;
-import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
 import '../core/constants/app_constants.dart';
 import '../data/local/shared_prefs.dart';
 import 'app_url.dart';
@@ -25,9 +23,7 @@ class ApiClient extends GetxService {
     bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
 
     var mainHeaders = {
-      // 'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Type': 'application/json',
-
       'Authorization': 'Bearer $bearerToken'
     };
     try {
@@ -35,46 +31,13 @@ class ApiClient extends GetxService {
 
       http.Response response = await client
           .get(
-        Uri.parse(ApiUrl.baseUrl + uri),
-        headers: headers ?? mainHeaders,
-      )
+            Uri.parse(ApiUrl.baseUrl + uri),
+            headers: headers ?? mainHeaders,
+          )
           .timeout(const Duration(seconds: timeoutInSeconds));
       return handleResponse(response, uri);
     } catch (e) {
       debugPrint('------------>>>${e.toString()}');
-      return const Response(statusCode: 1, statusText: noInternetMessage);
-    }
-  }
-
-  ///================================================================patch Method============================///
-  static Future<Response> patchData(
-      String uri,
-      dynamic body, {
-        Map<String, String>? headers,
-        bool isBody = true,
-      }) async {
-    bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-
-    var mainHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $bearerToken'
-    };
-    try {
-      debugPrint(
-          '====> API Call: ${ApiUrl.baseUrl}$uri\nHeader: ${headers ?? mainHeaders}');
-      debugPrint('====> API Body: $body');
-
-      http.Response response = await client
-          .patch(
-        Uri.parse(ApiUrl.baseUrl + uri),
-        body: isBody ? body : null,
-        headers: headers ?? mainHeaders,
-      )
-          .timeout(const Duration(seconds: timeoutInSeconds));
-      return handleResponse(response, uri);
-    } catch (e) {
-      debugPrint('Error------------${e.toString()}');
-
       return const Response(statusCode: 1, statusText: noInternetMessage);
     }
   }
@@ -95,245 +58,15 @@ class ApiClient extends GetxService {
 
       http.Response response = await client
           .post(
-        Uri.parse(ApiUrl.baseUrl + uri),
-        body: body,
-        headers: headers ?? mainHeaders,
-      )
+            Uri.parse(ApiUrl.baseUrl + uri),
+            body: body,
+            headers: headers ?? mainHeaders,
+          )
           .timeout(const Duration(seconds: timeoutInSeconds));
       return handleResponse(response, uri);
     } catch (e) {
       debugPrint('Error------------${e.toString()}');
 
-      return const Response(statusCode: 1, statusText: noInternetMessage);
-    }
-  }
-
-  //>>>>>>>>>>>>>>>>>>✅✅Post Multipart✅✅<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-  static Future<Response> postMultipartData(
-      String uri, Map<String, dynamic> body,
-      {List<MultipartBody>? multipartBody,
-        String requestType = "POST",
-        Map<String, String>? headers}) async {
-    try {
-      bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-
-      var mainHeaders = {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $bearerToken'
-      };
-
-      debugPrint('====> API Call: $uri\nHeader: ${headers ?? mainHeaders}');
-      debugPrint('====> API Body: $body with ${multipartBody?.length} picture');
-
-      var request =
-      http.MultipartRequest(requestType, Uri.parse(ApiUrl.baseUrl + uri));
-
-      // ✅ Convert `body` to `Map<String, String>`
-      Map<String, String> stringBody = body.map((key, value) =>
-          MapEntry(key, value is List || value is Map ? jsonEncode(value) : value.toString())
-      );
-
-      request.fields.addAll(stringBody); // ✅ Now it will work
-
-      if (multipartBody != null && multipartBody.isNotEmpty) {
-        for (var element in multipartBody) {
-          debugPrint("path : ${element.file.path}");
-
-          var mimeType = lookupMimeType(element.file.path) ?? 'application/octet-stream';
-          debugPrint("MimeType================$mimeType");
-
-          var multipartImg = await http.MultipartFile.fromPath(
-            element.key,
-            element.file.path,
-            contentType: MediaType.parse(mimeType),
-          );
-          request.files.add(multipartImg);
-        }
-      }
-
-      request.headers.addAll(mainHeaders);
-      http.StreamedResponse response = await request.send();
-      final content = await response.stream.bytesToString();
-      debugPrint('====> API Response: [${response.statusCode}] $uri\n$content');
-
-      return Response(
-          statusCode: response.statusCode,
-          statusText: noInternetMessage,
-          body: content);
-    } catch (e) {
-      debugPrint('------------${e.toString()}');
-
-      return const Response(statusCode: 1, statusText: noInternetMessage);
-    }
-  }
-
-
-
-  ///====================================What===================
-  static Future<Response> patch({
-    required String url,
-    required Map<String, String> body,
-    File? file, // Optional file
-    String fileKey = 'file', // Default file field key
-    Map<String, String>? headers,
-  }) async {
-    try {
-      // Bearer token setup
-      String? bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-      var defaultHeaders = {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $bearerToken',
-      };
-
-      // Merge default headers with custom headers
-      var requestHeaders = {...defaultHeaders, if (headers != null) ...headers};
-
-      // Create Multipart Request
-      var request = http.MultipartRequest('PATCH', Uri.parse(ApiUrl.baseUrl + url));
-      request.fields.addAll(body);
-      request.headers.addAll(requestHeaders);
-
-      // Add file if provided
-      if (file != null && file.existsSync()) {
-        var mimeType = lookupMimeType(file.path);
-        if (mimeType != null) {
-          var multipartFile = await http.MultipartFile.fromPath(
-            fileKey,
-            file.path,
-            contentType: MediaType.parse(mimeType),
-          );
-          request.files.add(multipartFile);
-        }
-      }
-
-      // Send Request
-      var response = await request.send();
-      final responseBody = await response.stream.bytesToString();
-
-      return Response(
-        statusCode: response.statusCode,
-        body: responseBody,
-        statusText: response.statusCode == 200 ? "success" : "error",
-      );
-    } catch (e) {
-      return Response(
-        statusCode: 500,
-        statusText: e.toString(),
-      );
-    }
-  }
-
-
-  ///=============================Put data===================
-
-  Future<Response> putData(String uri, dynamic body,
-      {Map<String, String>? headers}) async {
-    bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-
-    var mainHeaders = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Bearer $bearerToken'
-    };
-    try {
-      debugPrint('====> API Call: $uri\nHeader: ${headers ?? mainHeaders}');
-      debugPrint('====> API Body: $body');
-
-      http.Response response = await http
-          .put(
-        Uri.parse(ApiUrl.baseUrl + uri),
-        body: jsonEncode(body),
-        headers: headers ?? mainHeaders,
-      )
-          .timeout(const Duration(seconds: timeoutInSeconds));
-      return handleResponse(response, uri);
-    } catch (e) {
-      return const Response(statusCode: 1, statusText: noInternetMessage);
-    }
-  }
-
-
-//>>>>>>>>>>>>>>>>>>✅✅Patch Multipart✅✅<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  static Future<Response> patchMultipartData(String uri,Map<String, dynamic> body,
-      {List<MultipartBody>? multipartBody,
-        bool haveImage = true,
-        Map<String, String>? headers}) async {
-    try {
-      bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-
-      var mainHeaders = {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $bearerToken'
-      };
-
-      debugPrint('====> API Call: $uri\nHeader: ${headers ?? mainHeaders}');
-      debugPrint('====> API Body: $body with ${multipartBody?.length} picture');
-
-
-      var request =
-      http.MultipartRequest('PATCH', Uri.parse(ApiUrl.baseUrl + uri));
-
-      if (haveImage) {
-        // ignore: avoid_function_literals_in_foreach_calls
-        multipartBody?.forEach((element) async {
-          debugPrint("path : ${element.file.path}");
-
-          var mimeType = lookupMimeType(element.file.path);
-
-          debugPrint("MimeType================$mimeType");
-
-          var multipartImg = await http.MultipartFile.fromPath(
-            element.key,
-            element.file.path,
-            contentType: MediaType.parse(mimeType!),
-          );
-          request.files.add(multipartImg);
-          //request.files.add(await http.MultipartFile.fromPath(element.key, element.file.path,contentType: MediaType('video', 'mp4')));
-        });
-      }
-
-      request.headers.addAll(mainHeaders);
-      http.StreamedResponse response = await request.send();
-      final content = await response.stream.bytesToString();
-      debugPrint(
-          '====> API Response: [${response.statusCode}}] $uri\n$content');
-
-      return Response(
-          statusCode: response.statusCode,
-          statusText: "somethingWentWrong",
-          body: content);
-    } catch (e) {
-      debugPrint('------------${e.toString()}');
-
-      return const Response(statusCode: 1, statusText: "somethingWentWrong");
-    }
-  }
-
-
-
-
-
-
-
-
-  static Future<Response> deleteData(String uri,
-      {Map<String, String>? headers, dynamic body}) async {
-    bearerToken = await SharePrefsHelper.getString(AppConstants.bearerToken);
-
-    var mainHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': '$bearerToken'
-    };
-    try {
-      debugPrint('====> API Call: $uri\nHeader: ${headers ?? mainHeaders}');
-      debugPrint('====> API Call: $uri\n Body: $body');
-
-      http.Response response = await http
-          .delete(Uri.parse(ApiUrl.baseUrl + uri),
-          headers: headers ?? mainHeaders, body: body)
-          .timeout(const Duration(seconds: timeoutInSeconds));
-      return handleResponse(response, uri);
-    } catch (e) {
       return const Response(statusCode: 1, statusText: noInternetMessage);
     }
   }
@@ -386,5 +119,6 @@ class MultipartBody {
 class MultipartListBody {
   String key;
   String value;
+
   MultipartListBody(this.key, this.value);
 }
